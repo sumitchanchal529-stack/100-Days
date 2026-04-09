db5b05867fb27810140adcf89920412cf670c2ad
Sumit Chanchal <Sumitchanchal529@gmail.com>
2026-04-09 11:55:29 +0530

    updating the file

---
+++ Day9/Dictionaries.py
@@ -0,0 +1,9 @@
+colours = {
+    "Apple" : "Red",
+    "Orange": "Orange",
+    "banana" : "Yellow"
+}
+print(colours["Apple"])
+
+for key in colours:
+    print(colours[key])
---
+++ Day9/Nesting.py
@@ -0,0 +1,15 @@
+capitals = {
+    "France" : "Paris",
+    "Germany" : "Berlin",
+
+}
+# Nested List in Distionary
+
+travel_log = {
+    "France "  : ["Paris", "Lille", "Dijon"],
+    "Germany" : ["Stuttgart", "Berlin"],
+    "India" : ["Delhi", "Mumbai", "Bhopal"]
+}
+print(travel_log["France "][1])
+
+nested_list = ["A", "B" ,["C","Dgit "]]
